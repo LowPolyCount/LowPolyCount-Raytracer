316765cfbcf0e8edf38af4bdf0a2fef353524f0e
Lowpolycount <tradewars_org@hotmail.com>
2015-01-17 10:07:40 -0500

    Project should now compile and run properly in release and debug

--- lib/yaml-cpp-0.5.1/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Joel/Desktop/code/lib/yaml-cpp-0.5.1
-# Build directory: C:/Users/Joel/Desktop/code/lib/yaml-cpp-0.5.1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(test)
-subdirs(util)
